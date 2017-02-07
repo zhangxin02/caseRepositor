70febb469e7aa43807c01b5552984daf72c5e282
1111 <111>
2017-02-07 19:26:26 +0800

    angular初步了解/自定义指令

---
+++ angular01/02指令/js/common.js
@@ -0,0 +1,4 @@
+/**
+ * Created by leson on 2017/2/7.
+ */
+var app = angular.module("app", []);
---
+++ angular01/02指令/js/headerdirective.js
@@ -0,0 +1,8 @@
+/**
+ * Created by leson on 2017/2/7.
+ */
+app.directive("myheader", function () {
+    return {
+        template:"<ul><li>首页</li><li>商品详情</li></ul>"
+    }
+});
